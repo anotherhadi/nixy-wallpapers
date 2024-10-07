c200808af2fc37ffe389340d4a973da1e5bd7c54
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-10-07 17:54:10 +0200

    Update

--- docs/create_preview.sh
+++ docs/create_preview.sh
@@ -1,24 +1,54 @@
 #!/usr/bin/env bash
 
-# Create WALLPAPERS.md to preview wallpapers in ./wallpapers
-
+# Desktop view (3 column)
 WALLPAPERS_FOLDER="."
 WALLPAPERS_MD="./README.md"
 [[ -d "$WALLPAPERS_FOLDER" ]] || (echo "Wallpapers folder not found" && exit 1)
 
-echo "# Nixy Wallpapers" >"$WALLPAPERS_MD"
-echo "" >>"$WALLPAPERS_MD"
-printf "A collection of wallpapers for [Nixy](https://github.com/anotherhadi/nixy)." >>"$WALLPAPERS_MD"
-echo "" >>"$WALLPAPERS_MD"
-echo "" >>"$WALLPAPERS_MD"
-echo "| Preview |" >>"$WALLPAPERS_MD"
-echo "| -- |" >>"$WALLPAPERS_MD"
+cat <<EOT >"$WALLPAPERS_MD"
+# Nixy Wallpapers
+
+A collection of wallpapers for [Nixy](https://github.com/anotherhadi/nixy).
+See the [mobile readme (1 columns)](./docs/MOBILE-VIEW.md)
+
+
+| Preview |    |    |
+| ------- | -- | -- |
+EOT
+
+# Put the wallpapers in 3 columns
+i=0
+for file in "$WALLPAPERS_FOLDER"/*.png; do
+  filename=$(basename "$file")
+  printf "| ![$filename](./$filename) $filename " >>"$WALLPAPERS_MD"
+  i=$((i + 1))
+  if [ "$i" -eq 3 ]; then
+    echo "|" >>"$WALLPAPERS_MD"
+    i=0
+  fi
+done
+if [ "$i" -ne 0 ]; then
+  echo "|" >>"$WALLPAPERS_MD"
+fi
+
+# Mobile view (1 column)
+
+WALLPAPERS_FOLDER="."
+WALLPAPERS_MD="./docs/MOBILE-VIEW.md"
+[[ -d "$WALLPAPERS_FOLDER" ]] || (echo "Wallpapers folder not found" && exit 1)
+
+cat <<EOT >"$WALLPAPERS_MD"
+# Nixy Wallpapers
+
+A collection of wallpapers for [Nixy](https://github.com/anotherhadi/nixy).
+See the [desktop readme (3 columns)](../README.md)
+
+
+| Preview |
+| ------- |
+EOT
 
 for file in "$WALLPAPERS_FOLDER"/*.png; do
   filename=$(basename "$file")
   echo "| ![$filename](./$filename) $filename |" >>"$WALLPAPERS_MD"
 done
-
-# Format
-pandoc -t commonmark_x "$WALLPAPERS_MD" -o "/tmp/wallpapers.md"
-mv "/tmp/wallpapers.md" "$WALLPAPERS_MD"
