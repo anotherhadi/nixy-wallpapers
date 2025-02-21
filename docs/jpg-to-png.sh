f3a1bf9751ef5cee0932adfeaf8dd094e1e78f0b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-02-21 20:03:14 +0100

    add quote

--- docs/jpg-to-png.sh
+++ docs/jpg-to-png.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cd wallpapers || exit 1
-for img in *.jpg; do
+for img in *".jpg"; do
   convert "$img" "${img%.jpg}.png"
   rm "$img"
 done
 
-for img in *.jpeg; do
+for img in *".jpeg"; do
   convert "$img" "${img%.jpeg}.png"
   rm "$img"
 done
